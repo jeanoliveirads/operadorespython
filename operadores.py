2ea3b688ea8db1e7239ed6a6475a07134e2cd406
Jean Oliveira <99375625+jeanoliveirads@users.noreply.github.com>
2023-08-27 15:37:11 -0300

    Create operadores.py

---
+++ operadores.py
@@ -0,0 +1,38 @@
++   # Adição
+-   # Subtração
+*   # Multiplicação
+/   # Divisão
+%   # Módulo (resto da divisão)
+**  # Exponenciação
+//  # Divisão inteira
+==   # Igual a
+!=   # Diferente de
+<    # Menor que
+>    # Maior que
+<=   # Menor ou igual a
+>=   # Maior ou igual a
+
+Operadores Lógicos:
+Esses operadores realizam operações lógicas em valores booleanos.
+
+python
+Copy code
+and  # E lógico
+or   # OU lógico
+not  # NÃO lógico
+
+Operadores de Identidade:
+Esses operadores são usados para comparar objetos quanto à identidade.
+
+python
+Copy code
+is    # É o mesmo objeto
+is not  # Não é o mesmo objeto
+
+Operadores de Associação (Membership):
+Esses operadores verificam se um valor está presente em uma sequência (como lista, tupla, conjunto, etc.).
+
+python
+Copy code
+in    # Está presente na sequência
+not in  # Não está presente na sequência
