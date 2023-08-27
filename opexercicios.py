35f2f222ab76ebb8e1568e720f925a4d54dfd257
Jean Oliveira <99375625+jeanoliveirads@users.noreply.github.com>
2023-08-27 15:38:17 -0300

    Create opexercicios.py

---
+++ opexercicios.py
@@ -0,0 +1,91 @@
+amos ver alguns exemplos práticos de como usar os operadores em Python:
+
+Operadores Aritméticos:
+python
+Copy code
+# Adição
+resultado = 5 + 3  # resultado será igual a 8
+
+# Multiplicação
+produto = 4 * 6  # produto será igual a 24
+
+# Divisão
+quociente = 10 / 2  # quociente será igual a 5.0 (resultado é um float)
+
+# Módulo (resto da divisão)
+resto = 10 % 3  # resto será igual a 1
+
+# Exponenciação
+potencia = 2 ** 3  # potencia será igual a 8
+
+# Divisão inteira
+divisao_inteira = 15 // 2  # divisao_inteira será igual a 7
+Operadores de Comparação:
+python
+Copy code
+x = 5
+y = 10
+
+# Igual a
+igual = x == y  # igual será False
+
+# Diferente de
+diferente = x != y  # diferente será True
+
+# Menor que
+menor = x < y  # menor será True
+
+# Maior que
+maior = x > y  # maior será False
+
+# Menor ou igual a
+menor_igual = x <= y  # menor_igual será True
+
+# Maior ou igual a
+maior_igual = x >= y  # maior_igual será False
+Operadores Lógicos:
+python
+Copy code
+a = True
+b = False
+
+# E lógico (and)
+resultado_and = a and b  # resultado_and será False
+
+# OU lógico (or)
+resultado_or = a or b  # resultado_or será True
+
+# NÃO lógico (not)
+resultado_not = not a  # resultado_not será False
+Operadores de Atribuição:
+python
+Copy code
+x = 5
+
+# Soma e atribuição
+x += 3  # x será igual a 8
+
+# Multiplicação e atribuição
+x *= 2  # x será igual a 16
+
+# Divisão e atribuição
+x /= 4  # x será igual a 4.0
+
+# Módulo e atribuição
+x %= 3  # x será igual a 1.0
+
+# Exponenciação e atribuição
+x **= 2  # x será igual a 1.0
+
+# Divisão inteira e atribuição
+x //= 2  # x será igual a 0.0
+Operadores de Associação (Membership):
+python
+Copy code
+lista = [1, 2, 3, 4, 5]
+
+# Está presente na sequência (in)
+esta_na_lista = 3 in lista  # esta_na_lista será True
+
+# Não está presente na sequência (not in)
+nao_esta_na_lista = 6 not in lista  # nao_esta_na_lista será True
